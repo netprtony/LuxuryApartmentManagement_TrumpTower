2f0a5882a5cb0ffb8fb4e39983eb60aa3503ae98
netprtony <huynhvikhang6a13@gmail.com>
2024-05-04 16:11:23 +0700

    update login

--- LuxuryApartmentManagement_TrumpTower/src/DAO/AccountDAO.java
+++ LuxuryApartmentManagement_TrumpTower/src/DAO/AccountDAO.java
@@ -4,10 +4,36 @@
  */
 package DAO;
 
+import MODEL.Account;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 /**
  *
  * @author netprtony
  */
 public class AccountDAO {
-    
+    public  Account Login(String user, String password){
+        Account acc = null;
+        try {
+            Connection con = DBConnect.openConnection();
+            String sql = "exec USP_LoginAccount @username = ? , @password = ?";
+            PreparedStatement pre = con.prepareStatement(sql);
+            pre.setString(1, user);
+            pre.setString(2, password);
+            ResultSet r = pre.executeQuery();
+            if(r.next()){
+                acc = new Account();
+                acc.setUser(r.getString("Acc_User"));
+                acc.setName(r.getString("Acc_NameOwner"));
+                acc.setPassword(r.getString("Acc_Password"));
+                acc.setRole(r.getString("Acc_Role"));
+            }
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return acc;
+    }
 }
