57ade9fe248ffad1d29ff43fa36e47bb8f366e1d
netprtony <huynhvikhang6a13@gmail.com>
2024-05-06 18:54:44 +0700

    update buildingdao

--- LuxuryApartmentManagement_TrumpTower/src/DAO/BuildingDAO.java
+++ LuxuryApartmentManagement_TrumpTower/src/DAO/BuildingDAO.java
@@ -4,10 +4,66 @@
  */
 package DAO;
 
+import GUI.Building;
+import MODEL.BuildingModel;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author netprtony
  */
 public class BuildingDAO {
-    
+    public List<BuildingModel> readAll(){
+        List<BuildingModel> lst = new ArrayList<>();
+        try {
+            String  sql = "exec USP_GetAllBuilding";
+            Connection con = DBConnect.openConnection();
+            Statement stm = con.createStatement();
+            ResultSet rs = stm.executeQuery(sql);
+            while (rs.next()) {                
+                BuildingModel b = new BuildingModel();
+                b.setId(rs.getString(1));
+                b.setAddress(rs.getString(2));
+                b.setName(rs.getString(3));
+                b.setDescribe(rs.getString(4));
+                lst.add(b);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lst;
+    }
+    public List<BuildingModel> FindById(String id){
+        List<BuildingModel> lst = new ArrayList<>();
+        try {
+            String sql = "exec USP_FindByIDBuilding @id = ?";
+            Connection con = DBConnect.openConnection();
+            PreparedStatement pstm = con.prepareStatement(sql);
+            pstm.setString(1, id);
+            ResultSet rs = pstm.executeQuery();
+            lst.clear();
+            while(rs.next()){
+                BuildingModel b = new BuildingModel();
+                b.setId(rs.getString(1));
+                b.setAddress(rs.getString(2));
+                b.setName(rs.getString(3));
+                b.setDescribe(rs.getString(4));
+                lst.add(b);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return lst;
+    }
+    public int add(Building Bui){
+        try {
+            String sql = ""
+        } catch (Exception e) {
+        }
+    }
 }
